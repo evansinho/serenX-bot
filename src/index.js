a758ddc1932ded81220c6e9dcee7119faf89f450
evanson <igiri.evanson@gmail.com>
2021-02-06 23:14:41 +0100

    set up environment variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes/index.js';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.json({ message: 'Hello'})
 })
+
+app.use('/api', routes);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const router = express.Router();
+
+dotenv.config();
+
+
+
+export default router;
